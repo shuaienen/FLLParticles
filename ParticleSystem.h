8086a2d71486a42197904cf960624b0200ed1c6a
reGer_motk <maoruichen@163.com>
2019-03-05 17:11:28 +0800

    Update ParticleSystem.h

--- ParticleSystem.h
+++ ParticleSystem.h
@@ -1,14 +1,3 @@
-/*
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
-
 #ifndef __PARTICLESYSTEM_H__
 #define __PARTICLESYSTEM_H__
 
